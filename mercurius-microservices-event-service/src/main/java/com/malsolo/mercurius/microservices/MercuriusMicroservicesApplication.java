0ef7d57e1273a2373307c1d25e56f22c3be62f77
Javier Beneito Barquero <jbbarquero@gmail.com>
2015-10-01 11:30:54 +0200

    Boot project created with basic maven structure and a basic project

---
+++ mercurius-microservices-event-service/src/main/java/com/malsolo/mercurius/microservices/MercuriusMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.mercurius.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuriusMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercuriusMicroservicesApplication.class, args);
+    }
+}
