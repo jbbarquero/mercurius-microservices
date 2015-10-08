b3d14c1385b094f68e26ba6d85fb61512642ca5a
Javier Beneito Barquero <jbbarquero@gmail.com>
2015-10-08 06:54:46 +0200

    Hystrix skeleton

---
+++ mercurius-microservices-hystrix-dashboard-service/src/main/java/com/malsolo/mercurius/microservices/hystrix/MercuriusHystrixDashboardServiceApplication.java
@@ -0,0 +1,23 @@
+package com.malsolo.mercurius.microservices.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableHystrixDashboard
+@SpringBootApplication
+public class MercuriusHystrixDashboardServiceApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix/index.html";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+				MercuriusHystrixDashboardServiceApplication.class, args);
+	}
+}
---
+++ mercurius-microservices-hystrix/src/main/java/com/malsolo/mercurius/microservices/hystrix/client/MercuriusMicroservicesHystrixApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.mercurius.microservices.hystrix.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuriusMicroservicesHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercuriusMicroservicesHystrixApplication.class, args);
+    }
+}
